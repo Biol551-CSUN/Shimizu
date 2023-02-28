9a62d14739af6f04b8f5b2748b7095c726442f5c
Kenta Shimizu <kenta.shimizu.678@my.csun.edu>
2023-02-28 12:33:22 -0800

    add

--- Week_05/script/Homework20230223.R
+++ Week_05/script/Homework20230223.R
@@ -1,17 +1,62 @@
+### Library data ###
+install.packages("lubridate")
 library(tidyverse)
 library(here)
 library(lubridate)
 
-graph_CondData <- read_csv(here("Week_05","data","CondData.csv"))
+### Data analysis ###
+ymd("2021-02-24")
+datetimes<-c("02/24/2021 22:22:20",
+             "02/25/2021 11:21:10",
+             "02/26/2021 8:01:52")
+datetimes <- mdy_hms (datetimes)
+
+month(datetimes, label = TRUE)
+day(datetimes)
+hour(datetimes)
+datetimes + hours(4)
+datetimes + days(2) ## adds 2 days ###
 
 
-graph_CondData <- graph_CondData%>%
-mutate(Date = mdy_hm(Date))
+
+
+
+
+
+view(CondData)
+
+
+###Data read in ####
+CondData<-read_csv(here("Week_05","Data", "CondData.csv"))
+
+CondData <- CondData %>%
+  mutate(Date = mdy_hm(Date)) %>%  ## correcting date and time format ###
+  round_date(CondData, "10 seconds")   ### rounding to 10s #
+
+### read in the Data ###
+DepthData<-read_csv(here("Week_05","Data", "DepthData.csv"))
+
+DepthData <- DepthData %>%
+  mutate(Date = mdy_hm(Date)) %>%
   
-  ##  mutate(date = as.character(date),
-         date = myd)
+  inner_join(CondData) %>% ##### joining the data #### 
 
-
-view(graph_CondData)
-
+### renaming the new joined data to save confusion ###
+mutate(Date = round_date(Date, "2 minute")) %>%     ### rounding date ###
+  group_by(Date) %>%      ### group by date so can produce plot ###
   
+  summarise(mean_depth = mean(Depth, na.rm = TRUE),
+            mean_temp = mean(TempInSitu, na.rm = TRUE),
+            mean_salinity = mean(Salinity, na.rm = TRUE))
+
+ggplot(data = DepthData,  ## produce plot ##
+       mapping = aes(x = Date,
+                     y = mean_temp,
+                     color = Date))+
+  
+  geom_point()+
+  geom_smooth(method = "lm")+ #line of best fit##
+  labs(x = "Time",     #axis titles##
+       y = "Temp")
+
+ggsave(here("Week_05", "Output", "DepthData_homework.png"))  ##save data##
